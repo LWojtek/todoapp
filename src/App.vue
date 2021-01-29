3c8f5062223afcd0ec55639a97b212e30e94f257
WOJCIECH LEHMANN <lehmann.wojciech@icloud.com>
2021-01-29 11:16:14 +0100

    add todos api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos :todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todos from './components/Todos.vue'
+import Header from './components/layout/Header.vue'
+import AddTodo from './components/AddTodo.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+  },
+  methods: {
+    deleteTodo(id){
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then( this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err))     
+    },
+    addTodo(newTodo){
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+       title,
+       completed})
+       .then(res =>  this.todos = [...this.todos, res.data])
+       .catch(err => console.log(err))
+     
+    }
+  },
+  created(){
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=8')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  },
+  data() {
+    return {
+      todos: [
+      
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    outline: none;
+    background: #555;
+    color: #fff;
+    padding: 12px 20px;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-size: 18px;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+
+
 </style>
